8a23ba234b9bd9a168ffcccd171b607a43625b12
Pu <pu.mb@qq.com>
2021-06-05 20:07:46 +0800

    修改字体及配色

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Sarasa Mono SC:style=Bold Italic:size=14" };
+static const char dmenufont[]       = "Sarasa Mono SC:style=Bold Italic:size=14";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#8ec07c";
+static const char col_gray3[]       = "#a89984";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_cyan[]        = "#689d6a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
